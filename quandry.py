7978d52054cc97254d3b7fcbc6c91dee192d24cb
DanCardin <ddcardin@gmail.com>
2016-03-01 13:13:06 -0500

    Add base quandry.py

---
+++ quandry.py
@@ -0,0 +1,167 @@
+import functools
+
+
+class Query:
+    def __init__(self, *args, parent=None):
+        self._result = None
+        self._cached_result = []
+        self._parent = parent
+        self._actions = {arg.__name__: arg for arg in args}
+
+    @property
+    def result(self):
+        result = self._result
+        if result is None and self._parent:
+            result = self._parent.result
+        return result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
+
+    def __getattr__(self, name):
+        if name in self._actions:
+            return self._actions[name](self)
+        raise AttributeError()
+
+    def __call__(self, obj):
+        self._result = obj
+        self._cached_result = []
+        return self
+
+    def __iter__(self):
+        if self._cached_result:
+            for r in self._cached_result:
+                yield r
+            return None
+        for r in self.result:
+            self._cached_result.append(r)
+            yield r
+
+
+class Value():
+    """Generate deferred comparison object.
+    """
+    def __init__(self, _getattr=None):
+        self._getattr = _getattr
+
+    def _check_attr(self, comp):
+        def check(x):
+            return comp(self._getattr(x))
+        if self._getattr:
+            return check
+        return comp
+
+    def __eq__(self, other):
+        def comp(x):
+            return x == other
+        return self._check_attr(comp)
+
+    def __lt__(self, other):
+        def comp(x):
+            return x < other
+        return self._check_attr(comp)
+
+    def __gt__(self, other):
+        def comp(x):
+            return x > other
+        return self._check_attr(comp)
+
+    def __getattr__(self, name):
+        def _getattr(obj):
+            if self._getattr:
+                obj = self._getattr(obj)
+            return getattr(obj, name)
+        return type(self)(_getattr)
+
+
+def queryable(func):
+    """Mark a function as able to be directly used by a `Query`.
+
+    A convenience decorator for functions that take the arguments:
+        result(Iterable): the iterable results on which the function operates
+        *args: arbitrary positional args
+        **kwargs: arbitrary positional keyword args
+
+    and returns the results of that operation.
+    """
+    @functools.wraps(func)
+    def query_wrapper(self):
+        def func_wrapper(*args, **kwargs):
+            query = type(self)(*self._actions.values(), parent=self)
+            result = func(self, *args, **kwargs)
+            query.result = result
+            return query
+        return func_wrapper
+    return query_wrapper
+
+
+@queryable
+def filter(result, *args, **kwargs):
+    """Filter out elements of the `result`.
+    """
+    for value in args:
+        if callable(value):
+            result = (r for r in result if value(r))
+
+    for key, value in kwargs.items():
+        if callable(value):
+            result = (r for r in result if value(r))
+
+    return result
+
+
+@queryable
+def exclude(result, *args, **kwargs):
+    """Exclude elements of the `result`.
+    """
+    for value in args:
+        result = (r for r in result if r != value)
+
+    for key, value in kwargs.items():
+        if callable(value):
+            result = (r for r in result if value(r))
+
+    return result
+
+
+value = Value()
+Q = Query(filter, exclude)
+
+woah = [1, 2, 3, 4, 5]
+result = Q(woah).filter(value > 2)
+result2 = result.filter(value > 3)
+result3 = result2.exclude(5)
+
+
+class Bleh:
+    def __init__(self, value):
+        self.prop = value
+
+    def __repr__(self):
+        return str(self.prop)
+
+assert list(result) == [3, 4, 5], list(result)
+assert list(result) == [3, 4, 5], list(result)
+assert list(result2) == [4, 5], list(result2)
+assert list(result3) == [4], list(result3)
+
+
+woah = [Bleh(1), Bleh(2), Bleh(3), Bleh(4), Bleh(5)]
+result = Q(woah).filter(value.prop > 2)
+result2 = result.filter(value.prop > 3)
+assert list(result) == woah[2:], list(result)
+assert list(result2) == woah[3:], list(result2)
+
+
+woah = [Bleh(Bleh(1)), Bleh(Bleh(5))]
+result = Q(woah).filter(value.prop.prop > 2)
+assert list(result) == woah[1:], list(result)
+
+
+woah = [Bleh(Bleh(Bleh(2))), Bleh(Bleh(Bleh(6)))]
+result = Q(woah).filter(value.prop.prop.prop > 3)
+assert list(result) == woah[1:], list(result)
+
+
+# woah2 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
